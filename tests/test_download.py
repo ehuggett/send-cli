c5ed9644db32683038eac96c1bf595b91097a892
ehuggett <ehuggett@users.noreply.github.com>
2017-10-15 00:28:54 +0100

    wip: add pytest checks

---
+++ tests/test_download.py
@@ -0,0 +1,30 @@
+from download import splitkeyurl
+def test_splitkeyurl():
+   url = 'https://send.firefox.com/download/c8ab3218f9/#39EL7SuqwWNYe4ISl2M06g'
+   prefix,urlid,key = splitkeyurl(url)
+   assert prefix == 'https://send.firefox.com/'
+   assert urlid == 'c8ab3218f9'
+   assert key == '39EL7SuqwWNYe4ISl2M06g'
+
+from download import key_decode
+def test_key_decode():
+   jwk = '39EL7SuqwWNYe4ISl2M06g'
+   assert key_decode(jwk) == b'\xdf\xd1\x0b\xed+\xaa\xc1cX{\x82\x12\x97c4\xea'
+
+
+from download import decrypt
+from hashlib import sha256
+def test_decrypt():
+   data = open('tests/data/1M', 'rb')
+   key = 'gb5eCcERV2EDqFB2WNR4kQ'
+   iv = '81be5e09c111576103a85076'
+#   tag = b'L\x1f\xf6\xe3\r\x94L\xd0 \x9b\\\xd1\xaf\xe3>K'
+   tag = b'\xd2\xdc\xb1\x0c\xb3D\xc5\xf5\xb3\xc80EOc`v'
+
+   plain = decrypt(data,key,iv,tag)
+
+   # The pointer should be at the beginning of the file
+   assert plain.tell() == 0
+
+   # To avoid storing the known-good plaintext for comparison check its sha256 hash
+   assert sha256(plain.read()).hexdigest() == 'cd55109593e01d4aec4a81ddcaf81d246dc9a17b7776c72cc1f8eead4d5457c0'
---
+++ tests/test_upload.py
@@ -0,0 +1,33 @@
+from upload import jwk_encode
+def test_jwk_encode():
+   key = b'\xdf\xd1\x0b\xed+\xaa\xc1cX{\x82\x12\x97c4\xea'
+   assert jwk_encode(key) == '39EL7SuqwWNYe4ISl2M06g'
+
+from upload import encrypt
+from hashlib import sha256
+def test_encrypt(monkeypatch):
+   def mockrandom(number_of_bytes):
+      return b'\x81\xbe^\t\xc1\x11Wa\x03\xa8PvX\xd4x\x91'[:number_of_bytes]
+
+   # use the same 'random' bytes every time (for encryption key and IV)
+   monkeypatch.setattr('upload.get_random_bytes', mockrandom)
+
+   fh = open('tests/data/1M', 'rb')
+   ciphertext,key,iv = encrypt(fh)
+
+   assert key == 'gb5eCcERV2EDqFB2WNR4kQ'
+   assert iv == '81be5e09c111576103a85076'
+
+   # The pointer should be at the beginning of the file
+   assert ciphertext.tell() == 0
+
+   # check the aes-gcm tag
+   ciphertext.seek(-16,2)
+   assert ciphertext.read() == b'L\x1f\xf6\xe3\r\x94L\xd0 \x9b\\\xd1\xaf\xe3>K'
+
+   # To avoid storing the known-good ciphertext for comparison check its sha256 hash, after removing appended gcm tag
+   ciphertext.seek(-16,2)
+   ciphertext.truncate()
+   ciphertext.seek(0)
+   assert sha256(ciphertext.read()).hexdigest() == 'cd55109593e01d4aec4a81ddcaf81d246dc9a17b7776c72cc1f8eead4d5457c0'
+
