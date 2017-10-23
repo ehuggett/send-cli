09b6d72bd9d1bd2eae1a55fde57fd7206a74ad6d
ehuggett <ehuggett@users.noreply.github.com>
2017-10-23 21:21:36 +0100

    fix indentation

--- sendclient/common.py
+++ sendclient/common.py
@@ -6,26 +6,26 @@ from tqdm import tqdm
 import requests
 
 def checkServerVersion(service, ignoreVersion=False):
-   if ignoreVersion == True:
-      return True
+    if ignoreVersion == True:
+        return True
 
-   r = requests.get(service + '__version__')
-   r.raise_for_status()
-   b = r.json()
-   if b['version'] == 'v1.2.3' and b['commit'] == 'c97abb4':
-      return True
-   else:
-      return False
+    r = requests.get(service + '__version__')
+    r.raise_for_status()
+    b = r.json()
+    if b['version'] == 'v1.2.3' and b['commit'] == 'c97abb4':
+        return True
+    else:
+        return False
 
 def fileSize(f):
-   '''returns the size of a file like object in bytes/octets'''
-   offset = f.tell()
+    '''returns the size of a file like object in bytes/octets'''
+    offset = f.tell()
 
-   f.seek(0, 2)
-   size = f.tell()
+    f.seek(0, 2)
+    size = f.tell()
 
-   f.seek(offset) # return to the previous position in the file
-   return size
+    f.seek(offset) # return to the previous position in the file
+    return size
 
 def progbar(total):
-   return tqdm(unit='B', unit_scale=True, total=total)
+    return tqdm(unit='B', unit_scale=True, total=total)
--- sendclient/upload.py
+++ sendclient/upload.py
@@ -11,54 +11,54 @@ from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
 from sendclient.common import *
 
 def jwk_encode(key):
-   '''base64 encode with url safe alphabet and strip padding'''
-   jwk = urlsafe_b64encode(key)
-   jwk = jwk.decode('utf-8').replace('=','')
-   return jwk
+    '''base64 encode with url safe alphabet and strip padding'''
+    jwk = urlsafe_b64encode(key)
+    jwk = jwk.decode('utf-8').replace('=','')
+    return jwk
 
 def encrypt(fh):
-   '''Encrypt data with the same method as the Send browser/js client'''
-   key = get_random_bytes(16)
-   iv = get_random_bytes(12)
-   ciphertext = SpooledTemporaryFile(max_size=SPOOL_SIZE, mode='w+b')
-   cipher = AES.new(key, AES.MODE_GCM, iv)
+    '''Encrypt data with the same method as the Send browser/js client'''
+    key = get_random_bytes(16)
+    iv = get_random_bytes(12)
+    ciphertext = SpooledTemporaryFile(max_size=SPOOL_SIZE, mode='w+b')
+    cipher = AES.new(key, AES.MODE_GCM, iv)
 
-   pbar = progbar(fileSize(fh))
+    pbar = progbar(fileSize(fh))
 
-   for chunk in iter(lambda: fh.read(CHUNK_SIZE), b'' ):
-      ciphertext.write(cipher.encrypt(chunk))
-      pbar.update(len(chunk))
+    for chunk in iter(lambda: fh.read(CHUNK_SIZE), b'' ):
+        ciphertext.write(cipher.encrypt(chunk))
+        pbar.update(len(chunk))
 
-   pbar.close()
-   ciphertext.write( cipher.digest() )
-   fh.close()
+    pbar.close()
+    ciphertext.write( cipher.digest() )
+    fh.close()
 
-   key = jwk_encode(key)
-   iv = iv.hex()
-   ciphertext.seek(0)
+    key = jwk_encode(key)
+    iv = iv.hex()
+    ciphertext.seek(0)
 
-   return ciphertext,key,iv
+    return ciphertext,key,iv
 
 
 def put(service,data,filename,iv):
-   ''' 
-      Uploads data to Send.
-      Caution! Data is uploaded as given, this function will not encrypt it for you
-   '''
+    '''
+       Uploads data to Send.
+       Caution! Data is uploaded as given, this function will not encrypt it for you
+    '''
 
-   if checkServerVersion( service.replace('api/upload','') ) == False:
-      print('\033[1;41m!!! Potentially incompatible server version !!!\033[0m')
+    if checkServerVersion( service.replace('api/upload','') ) == False:
+        print('\033[1;41m!!! Potentially incompatible server version !!!\033[0m')
 
-   filename = quote_plus(filename)
-   #nb the Content-Type is also "public" metadata
-   files = MultipartEncoder(fields={'file': (filename, data, 'application/octet-stream') } )
-   pbar = progbar(files.len)
-   monitor = MultipartEncoderMonitor(files, lambda files: pbar.update(monitor.bytes_read - pbar.n))
+    filename = quote_plus(filename)
+    #nb the Content-Type is also "public" metadata
+    files = MultipartEncoder(fields={'file': (filename, data, 'application/octet-stream') } )
+    pbar = progbar(files.len)
+    monitor = MultipartEncoderMonitor(files, lambda files: pbar.update(monitor.bytes_read - pbar.n))
 
-   public_meta = {'filename' : filename, 'id': iv }
-   headers = {'X-File-Metadata' : json.dumps(public_meta), 'Content-type' : monitor.content_type}
+    public_meta = {'filename' : filename, 'id': iv }
+    headers = {'X-File-Metadata' : json.dumps(public_meta), 'Content-type' : monitor.content_type}
 
-   r = requests.post(service, data=monitor, headers=headers, stream=True)
-   r.raise_for_status()
-   pbar.close()
-   return r.json()
+    r = requests.post(service, data=monitor, headers=headers, stream=True)
+    r.raise_for_status()
+    pbar.close()
+    return r.json()
