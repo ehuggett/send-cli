6c245b29c1441372bcc28580d7261638192f9fc4
ehuggett <ehuggett@users.noreply.github.com>
2017-10-23 23:36:50 +0100

    autoformat & fix indentation in tests

--- tests/create_testdata.py
+++ tests/create_testdata.py
@@ -5,11 +5,11 @@ import random
 # 085ff278c9d0c790f4a99a00917c35b5f9c6290130fb5e92c2ce0244d9e4049a  - data/128M
 
 random.seed(a='send-cli test data')
-data = bytearray(random.getrandbits(8) for _ in range(1024 * 1024) )
+data = bytearray(random.getrandbits(8) for _ in range(1024 * 1024))
 with open('data/1M', 'wb') as f:
-   f.write(data)
+    f.write(data)
 
-#random.seed(a='send-cli test data')
-#data = bytearray(random.getrandbits(8) for _ in range(1024 * 1024 * 128) )
-#with open('data/128M', 'wb') as f:
+# random.seed(a='send-cli test data')
+# data = bytearray(random.getrandbits(8) for _ in range(1024 * 1024 * 128) )
+# with open('data/128M', 'wb') as f:
 #   f.write(data)
--- tests/test_common.py
+++ tests/test_common.py
@@ -1,11 +1,11 @@
 from sendclient.common import fileSize
 def test_fileSize(testdata_1M):
-   # open the test file and move the pointer to check fileSize() does not change it
-   with open(str(testdata_1M), 'rb') as f:
-      f.seek(123)
+    # open the test file and move the pointer to check fileSize() does not change it
+    with open(str(testdata_1M), 'rb') as f:
+        f.seek(123)
 
-      # check the correct size is returned for the 1MiB file
-      assert fileSize(f) == 1024 * 1024
+        # check the correct size is returned for the 1MiB file
+        assert fileSize(f) == 1024 * 1024
 
-      # check the pointer has not moved
-      assert f.tell() == 123
+        # check the pointer has not moved
+        assert f.tell() == 123
