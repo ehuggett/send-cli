8dbeb96427e49d9e4fd2df64e3df813e4a30f6dc
ehuggett <ehuggett@users.noreply.github.com>
2017-10-24 02:27:11 +0100

    split encrypt() into encrypt_file() and encrypt_metadata()

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -3,30 +3,29 @@ def test_jwk_encode():
     key = b'\xdf\xd1\x0b\xed+\xaa\xc1cX{\x82\x12\x97c4\xea'
     assert jwk_encode(key) == '39EL7SuqwWNYe4ISl2M06g'
 
-from sendclient.upload import encrypt
+from sendclient.upload import encrypt_file
 from hashlib import sha256
-def test_encrypt(monkeypatch, testdata_1M):
-    def mockrandom(number_of_bytes):
-        return b'\x81\xbe^\t\xc1\x11Wa\x03\xa8PvX\xd4x\x91'[:number_of_bytes]
+def test_encrypt_file(testdata_1M):
 
-    # use the same 'random' bytes every time (for encryption key and IV)
-    monkeypatch.setattr('sendclient.upload.get_random_bytes', mockrandom)
+    class mock_secretKeys():
+        def __init__(self):
+            self.encryptKey = b'\x81\xbe^\t\xc1\x11Wa\x03\xa8PvX\xd4x\x91'
+            self.encryptIV = b'\x81\xbe^\t\xc1\x11Wa\x03\xa8Pv'
 
-    fh = open(str(testdata_1M), 'rb')
-    ciphertext, key, iv = encrypt(fh)
+    mock_keys = mock_secretKeys()
 
-    assert key == 'gb5eCcERV2EDqFB2WNR4kQ'
-    assert iv == '81be5e09c111576103a85076'
+    file = open(str(testdata_1M), 'rb')
+    encData, keys = encrypt_file(file, mock_keys)
 
     # The pointer should be at the beginning of the file
-    assert ciphertext.tell() == 0
+    assert encData.tell() == 0
 
     # check the aes-gcm tag
-    ciphertext.seek(-16, 2)
-    assert ciphertext.read() == b'L\x1f\xf6\xe3\r\x94L\xd0 \x9b\\\xd1\xaf\xe3>K'
+    encData.seek(-16, 2)
+    assert encData.read() == b'L\x1f\xf6\xe3\r\x94L\xd0 \x9b\\\xd1\xaf\xe3>K'
 
     # To avoid storing the known-good ciphertext for comparison check its sha256 hash, after removing appended gcm tag
-    ciphertext.seek(-16, 2)
-    ciphertext.truncate()
-    ciphertext.seek(0)
-    assert sha256(ciphertext.read()).hexdigest() == 'cd55109593e01d4aec4a81ddcaf81d246dc9a17b7776c72cc1f8eead4d5457c0'
+    encData.seek(-16, 2)
+    encData.truncate()
+    encData.seek(0)
+    assert sha256(encData.read()).hexdigest() == 'cd55109593e01d4aec4a81ddcaf81d246dc9a17b7776c72cc1f8eead4d5457c0'
