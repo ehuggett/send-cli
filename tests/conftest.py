00385ff1eb7731509c60ad914592bc7c3ec20402
ehuggett <ehuggett@users.noreply.github.com>
2017-10-27 21:13:18 +0100

    fix/update failing test for decrypt_file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,19 @@
-import random
 import pytest
-from hashlib import sha256
 
-@pytest.fixture(scope='session')
+import random
+import hashlib
+@pytest.fixture(scope='function')
 def testdata_1M(tmpdir_factory):
+    '''Creates and returns the path to the 1MB test file'''
     path = tmpdir_factory.mktemp('data').join('1M')
 
-    # save the current state of the random number generator
-    state = random.getstate()
-    # 'brake' the random number generator by always using the same seed value
-    random.seed(a='send-cli test data')
-    # get the test data
-    data = bytearray(random.getrandbits(8) for _ in range(1024 * 1024))
-    # restore the state of the rng from before we broke it
-    random.setstate(state)
+    # Create a new instance of random with a fixed seed
+    predictable = random.Random(x='send-cli test data')
+    data = bytearray(predictable.getrandbits(8) for _ in range(1024 * 1024))
 
-    assert sha256(data).hexdigest() == 'c957c34dad5b9b74e8a7ef2c4867b3a6163f2b2020ceb87cd78fa54fce1037de'
+    assert hashlib.sha256(data).hexdigest() == 'c957c34dad5b9b74e8a7ef2c4867b3a6163f2b2020ceb87cd78fa54fce1037de'
+
     with open(str(path), 'wb') as f:
         f.write(data)
 
-    return path
+    return str(path)
